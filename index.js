738941d7f19c5e24d1dc30b6d8bab351ec0522b6
Isaiah Jones <injones27@gmail.com>
2023-04-30 22:35:52 -0400

    Array find lab completed!

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin (record) {
+const stat = record.find(record => record.result ==="W");
+return stat ? stat.year : undefined;
+// if (stat === true) {
+//     return stat.year;
+// }
+// if (stat === false) {
+//     return undefined;
+// }
+}
